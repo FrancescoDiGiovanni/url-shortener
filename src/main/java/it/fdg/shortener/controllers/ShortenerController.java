b921fb8ce83dbba4f752d6bfb18757b71c2eccf4
YYI4809 <francesco.digiovanni@infocamere.it>
2025-11-15 12:41:46 +0100

    created api to retrieve original url from short id

--- src/main/java/it/fdg/shortener/controllers/ShortenerController.java
+++ src/main/java/it/fdg/shortener/controllers/ShortenerController.java
@@ -1,6 +1,16 @@
 package it.fdg.shortener.controllers;
 
+import it.fdg.shortener.dtos.UrlMappingDTO;
+import it.fdg.shortener.exceptions.ShortenerApiHttpStatusException;
+import it.fdg.shortener.services.ShortenerService;
+import it.fdg.shortener.utilities.Response;
+import it.fdg.shortener.utilities.ResponseUtility;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping(value = "shortener")
 public class ShortenerController {
+    @Autowired
+    private ShortenerService shortenerService;
+
+    @GetMapping(value = "/{shortId}", produces = "application/json")
+    public ResponseEntity<Response<UrlMappingDTO>> getOriginalUrl(@PathVariable String shortId) throws ShortenerApiHttpStatusException {
+        UrlMappingDTO urlMappingDTO = shortenerService.getOriginalUrlFromShortId(shortId);
+        if ( urlMappingDTO == null )
+            return ResponseUtility.buildErrorResponseEntity("SHORT0002", HttpStatus.NOT_FOUND, "No original url found with this short id.", null, log);
+
+        String okMessage = "Shortened url found successfully.";
+        return ResponseUtility.buildSuccessResponseEntity(okMessage, urlMappingDTO, log);
+    }
 }
--- src/test/java/it/fdg/shortener/controllers/ShortenerControllerTest.java
+++ src/test/java/it/fdg/shortener/controllers/ShortenerControllerTest.java
@@ -1,4 +1,64 @@
 package it.fdg.shortener.controllers;
 
+import it.fdg.shortener.dtos.UrlMappingDTO;
+import it.fdg.shortener.services.ShortenerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
 public class ShortenerControllerTest {
+    private MockMvc mockMvc;
+
+    @Mock
+    private ShortenerService shortenerService;
+
+    @InjectMocks
+    private ShortenerController controller;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    void getOriginalUrl_test001_givenShortIdFound_Returns200() throws Exception {
+        UrlMappingDTO dto = UrlMappingDTO.builder()
+                .shortId("10")
+                .originalUrl("http://foo.com")
+                .build();
+
+        when(shortenerService.getOriginalUrlFromShortId("10"))
+                .thenReturn(dto);
+
+        mockMvc.perform(get("/shortener/10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.payload.originalUrl").value("http://foo.com"));
+    }
+
+    @Test
+    void getOriginalUrl_test002_givenShortIdNotFound_Returns404() throws Exception {
+        when(shortenerService.getOriginalUrlFromShortId("10a"))
+                .thenReturn(null);
+
+        mockMvc.perform(get("/shortener/10a"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.error.code").value("SHORT0002"))
+                .andExpect(jsonPath("$.error.message")
+                        .value("No original url found with this short id."));
+    }
 }
